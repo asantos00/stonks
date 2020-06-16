648c8a7ff0a48bd0dd67602c3497ac4eecf50c62
Alexandre Santos <alexandre.santozz@gmail.com>
2020-06-16 20:33:30 +0100

    feat: add basic features and load credentials from env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Stonks 2020`,
+    description: `Triggered by Mr. PM`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -27,6 +27,52 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-google-spreadsheet",
+      options: {
+        // The `spreadsheetId` is required, it is found in the url of your document:
+        // https://docs.google.com/spreadsheets/d/<spreadsheetId>/edit#gid=0
+        spreadsheetId: "11KjAyvWG9b_-55axmOHQ7M9ZmEl4RNsZ-T4dsfpTFjw",
+
+        // The `spreadsheetName` is recommended, but optional
+        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
+        // If you are sourcing multiple sheets, you can set this to distringuish between the source data
+        spreadsheetName: "Stonks2020",
+
+        // The `typePrefix` is optional, default value is "GoogleSpreadsheet"
+        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
+        // It can be overridden to fully customize the root query
+        typePrefix: "GoogleSpreadsheet",
+
+        // The `credentials` are only needed when you need to be authenticated to read the document.
+        // It's an object with the following shape:
+        // {
+        //   client_email: "<your service account email address>",
+        //   private_key: "<the prive key for your service account>"
+        // }
+        //
+        // Refer to googles own documentation to retrieve the credentials for your service account:
+        //   - https://github.com/googleapis/google-api-nodejs-client#service-to-service-authentication
+        //   - https://developers.google.com/identity/protocols/OAuth2ServiceAccount
+        //
+        // When you have generated your credentials, it's easiest to refer to them from an environment variable
+        // and parse it directly:
+        credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS),
+
+        // Simple node transformation during node sourcing can be achieved by implementing the following functions
+        // - `filterNode`
+        // - `mapNode`
+        //
+        // By implementing a `filterNode(node): boolean` function, you can choose to eliminate some nodes before
+        // they're added to Gatsby, the default behaviour is to include all nodes:
+        filterNode: () => true,
+
+        // By implementing a `mapNode(node): node` function, you can provide your own node transformations directly
+        // during node sourcing, the default implementation is to return the node as is:
+        mapNode: node => node,
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,217 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const COLORS_BY_GROUP = {
+  TECH: "green",
+  GOODS: "red",
+  CRYPTO: "blue",
+  SPAIN: "yellow",
+  AVIATION: "orange",
+  GERMANY: "black",
+}
+
+const GroupTag = ({ group = "" }) => {
+  return (
+    <div
+      style={{
+        padding: "2px 8px",
+        borderRadius: "3px",
+        color: "white",
+        backgroundColor: COLORS_BY_GROUP[group] || "grey",
+        fontSize: 10,
+        marginRight: "auto",
+      }}
+    >
+      {group.toUpperCase()}
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  )
+}
+
+const FilterInput = ({ onKeyDown, value = "" }) => {
+  return (
+    <React.Fragment>
+      <label for="filter-by" style={{ marginRight: 16 }}>
+        Filter by:
+      </label>
+      <input
+        id="filter-by"
+        type="text"
+        value={value}
+        onChange={e => {
+          onKeyDown(e.target.value)
+        }}
+      />
+      <div style={{ fontSize: 10 }}>
+        Try out filters like "bull", "amd" or "tech"
+      </div>
+    </React.Fragment>
+  )
+}
+
+const StockCard = ({ stock }) => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      flexWrap: "wrap",
+      boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+      marginBottom: 16,
+      padding: 8,
+      overflow: "hidden",
+      borderRadius: 3,
+    }}
+  >
+    <GroupTag group={stock.group} />
+    <h3 style={{ paddingTop: 16 }}>
+      <a
+        href={stock.url}
+        style={{ textDecoration: "none", color: "#111" }}
+        target="_blank"
+      >
+        {stock.stock}
+      </a>
+    </h3>
+    <h4>
+      {stock.today}$ ({stock.drop})
+    </h4>
+    <h5>
+      Price earnings:{" "}
+      <span
+        style={{
+          color: stock.trend && stock.trend.includes("Bull") ? "green" : "red",
+        }}
+      >
+        {stock.p_e__priceEarnings_}
+      </span>
+    </h5>
+    <h5>
+      Strategy:{" "}
+      {stock.strategy && stock.strategy.includes("Pessimistic") ? "👎" : "👍"}
+    </h5>
+  </div>
 )
+
+const StockRow = ({ stock }) => {
+  const [isHover, setIsHover] = useState(false)
+
+  return (
+    <tr
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+      style={{
+        backgroundColor: isHover ? "#ddd" : "transparent",
+      }}
+    >
+      <td
+        style={{
+          minWidth: 110,
+        }}
+      >
+        <a href={stock.url} target="_blank">
+          {stock.stock}
+        </a>
+      </td>
+      <td>
+        {stock.today}$ ({stock.drop})
+      </td>
+      <td>
+        Price earnings:
+        <span
+          style={{
+            color:
+              stock.trend && stock.trend.includes("Bull") ? "green" : "red",
+          }}
+        >
+          {stock.p_e__priceEarnings_}
+        </span>
+      </td>
+      <td>
+        Strategy:{" "}
+        {stock.strategy && stock.strategy.includes("Pessimistic") ? "👎" : "👍"}
+      </td>
+    </tr>
+  )
+}
+
+const VISUALISATION_MODE = {
+  CARDS: "cards",
+  LIST: "list",
+}
+
+const IndexPage = ({ data }) => {
+  const [filterBy, setFilterBy] = useState("")
+  const [visualisationMode, setVisualisationMode] = useState(
+    VISUALISATION_MODE.CARDS
+  )
+  const filteredStocks = data.allGoogleSpreadsheetStonks2020Stonks.nodes.filter(
+    node => {
+      return (
+        (node.stock &&
+          node.stock.toLowerCase().includes(filterBy.toLowerCase())) ||
+        (node.trend &&
+          node.trend.toLowerCase().includes(filterBy.toLowerCase())) ||
+        (node.group &&
+          node.group.toLowerCase().includes(filterBy.toLowerCase()))
+      )
+    }
+  )
+
+  return (
+    <Layout>
+      <SEO title="Stonks - Overview" />
+      <main>
+        <FilterInput value={filterBy} onKeyDown={value => setFilterBy(value)} />
+        <button
+          style={{
+            marginBottom: 16,
+          }}
+          onClick={() =>
+            setVisualisationMode(
+              visualisationMode === VISUALISATION_MODE.CARDS
+                ? VISUALISATION_MODE.LIST
+                : VISUALISATION_MODE.CARDS
+            )
+          }
+        >
+          Change visualisation
+        </button>
+        {visualisationMode === VISUALISATION_MODE.LIST ? (
+          <table>
+            {filteredStocks.map(node => (
+              <StockRow stock={node} />
+            ))}
+          </table>
+        ) : (
+          filteredStocks.map(node => <StockCard stock={node} />)
+        )}
+      </main>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query OverviewQuery {
+    allGoogleSpreadsheetStonks2020Stonks {
+      nodes {
+        buy
+        chart
+        stock
+        today
+        tp__takeProfit_
+        trend
+        url
+        author
+        p_e__priceEarnings_
+        strategy
+        drop
+        group
+      }
+    }
+  }
+`
 
 export default IndexPage
